53fb3b8e502e473f2e87181bbcb451a156b1ccc0
mau2662 <vargas7384@gmail.com>
2021-03-07 03:06:19 +0000

    en proceso

---
+++ src/js/component/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-md navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				Start Bootstrap
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<ul className="navbar-nav mr-auto"></ul>
+
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<a className="nav-link">Login</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link">Login</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link">Login</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link">Login</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
