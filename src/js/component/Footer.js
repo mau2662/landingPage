972a8de1e2c49518551f9f9210021e4fc7def383
mau2662 <vargas7384@gmail.com>
2021-03-07 17:52:50 +0000

    listo

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-	return <p>Footer </p>;
+	return (
+		<div className="card text-white bg-dark mb-3">
+			<div className="card-body">
+				<h5 className="card-title"></h5>
+				<p className="card-text text-center">
+					Copyright @ Your Website 2021
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
